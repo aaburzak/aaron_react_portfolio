e02dd3f252c1f8943a9e365622dc7c69ce007eba
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-05-24 21:19:35 -0400

    began building portfolio, contact, and resume pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 function App() {
   return (
@@ -16,6 +19,18 @@ function App() {
               <Route 
                 path="/" 
                 element={<About />} 
+              />
+              <Route 
+                path="/portfolio" 
+                element={<Portfolio />} 
+              />
+              <Route 
+                path="/contact" 
+                element={<Contact />} 
+              />
+              <Route 
+                path="/resume" 
+                element={<Resume />} 
               />
             </Routes>
         </div>      
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Nav, Navbar, Container } from 'react-bootstrap';
-// import ProfilePic from "./ProfilePic";
+
 
 function Navigation() {
     return(
         <Navbar bg="dark" variant="dark">
         <Container>
-        {/* <ProfilePic/>  */}
-        <Navbar.Brand href="#home">Aaron Burzak</Navbar.Brand>
+        <Navbar.Brand href="/">Aaron Burzak</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/">About Me</Nav.Link>
-          <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
-          <Nav.Link href="#resume">Resume</Nav.Link>
+          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+          <Nav.Link href="/resume">Resume</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
