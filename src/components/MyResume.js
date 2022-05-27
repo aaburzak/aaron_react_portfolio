b47acd47ab1e342f0ff610b2575cfc724f60b8ac
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-05-27 16:28:54 -0400

    built out resume page

---
+++ src/components/MyResume.js
@@ -0,0 +1,9 @@
+import React from "react";
+import pic from "../images/aaronResume.png";
+
+
+function MyResume(){
+    return <img src={pic} />
+}
+
+export default MyResume
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { Card } from 'react-bootstrap';
+import { Card, Container, Row, Col } from 'react-bootstrap';
+import MyResume from "../components/MyResume";
 
 function Resume(){
     return(
-        <Card style={{ width: '18rem' }}>
-    <Card.Body className="aboutCard">
-      <Card.Title>Card Title</Card.Title>
-      <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-      <Card.Text>
-        Some quick example text to build on the card title and make up the bulk of
-        the card's content.
-      </Card.Text>
-      <Card.Link href="#">Card Link</Card.Link>
-      <Card.Link href="#">Another Link</Card.Link>
-    </Card.Body>
-  </Card>
+      <Container >
+      <Row>
+        <Col className="portHead">
+          <h3>My Resume</h3>
+          <p>For those interested in my professional experience in and out of coding</p>
+        </Col>
+      </Row>
+      <div class="col projectCard">
+        <Card>
+          <MyResume/>
+        </Card>
+      </div>
+      </Container>
     );
 };
 
