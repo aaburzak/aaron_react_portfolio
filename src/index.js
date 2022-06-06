782d1ce86a08f3cca0d2f2e984b1f56e20507e61
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-06-06 17:42:02 -0400

    set back to browser router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { HashRouter} from 'react-router-dom';
+// import { HashRouter} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,9 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    {/* <HashRouter> */}
     <App />
-    </HashRouter>
+    {/* </HashRouter> */}
   </React.StrictMode>
 );
 
