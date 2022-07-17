4ed9fd1508841b5a4498d93fa2c662f641ce7a28
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-07-17 12:45:48 -0400

    finalized styling changes for About.js page

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ return(
   <Container className="aboutMe">
   <Row>
       <Col className="portHead">
-        <h3>Aaron</h3>
+        <h1>About Me</h1>
       </Col>
     </Row>
   <Row className="picRow">
@@ -18,7 +18,7 @@ return(
         <ProfilePic/>
     </Col>
     </Row>
-    <Row className="justify-content-md-center">
+    <Row className="justify-content-md-center aboutRow">
       <Col className="md-auto aboutCol">
       <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
