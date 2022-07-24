1da68b5fbfbc1033d079593d972cefd005057cd5
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-07-24 17:36:18 -0400

    altered margins for contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -16,12 +16,16 @@ const Contact = () => {
         </Col>
       </Row>
       <Row className="contactRow">
-      <p className="text-center">Please reach out if you have interest in my work</p>
+      {/* <p className="text-center">Please reach out if you have interest in my work</p>
+      <p className="text-center">I would love to connect</p> */}
     <Col className="contactBox text-center">
     
-                <a  variant="port" href="mailto:aaron.j.burzak@gmail.com"><EmailPic/></a>{' '}
+                <a  href="mailto:aaron.j.burzak@gmail.com"><EmailPic/></a>{' '}
+                <p className="contactText">E-mail</p>
                 <a href="https://github.com/aaburzak"><GitHubPic/></a>{' '}
+                <p className="contactText">GitHub</p>
                 <a href="https://www.linkedin.com/in/aaron-burzak-53187654/"><LinkedinPic/></a>{' '}
+                <p className="contactText">LinkedIn</p>
   
     </Col>
     </Row>
