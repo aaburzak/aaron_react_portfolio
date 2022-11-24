1827d6ca35a98d72f1a82349966996a3ef46416c
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-11-24 13:11:18 -0500

    commented out redundant routes

--- src/App.js
+++ src/App.js
@@ -17,9 +17,6 @@ function App() {
         <div className="container main-content">
           <Routes>
             <Route path="/" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            {/* <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} /> */}
           </Routes>
         </div>
         <Footer />
