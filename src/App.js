8228da07fc358141b23bbcab2213ca525eb50b94
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-11-24 13:28:54 -0500

    removed script as it was causing a new error

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
-import Contact from "./pages/Contact";
-import Resume from "./pages/Resume";
+// import Portfolio from "./pages/Portfolio";
+// import Contact from "./pages/Contact";
+// import Resume from "./pages/Resume";
 import Footer from "./components/Footer";
 
 function App() {
